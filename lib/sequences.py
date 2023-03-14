6f962040ac068b03308a933b9492634547a2aece
Henry <henrysungyun@gmail.com>
2023-03-13 22:40:43 -0700

    finish

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+  fibo_array = []
+  if length == 0:
+    print(fibo_array)
+  elif length == 1:
+    fibo_array = [0]
+    print(fibo_array)
+  else:
+    fibo_array = [0, 1]
+    for i in range(2, length):
+        fibo_array.append(fibo_array[-1] + fibo_array[-2])
+    print(fibo_array)
